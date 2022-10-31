9a70fecd58e5b8037e6e2f2b783a321a55a6abd6
Simon Leier <simonleier@gmail.com>
2022-10-31 15:04:30 +0100

    New version

--- honey/CHANGELOG.md
+++ honey/CHANGELOG.md
@@ -1,10 +1,21 @@
 ## 0.1.0
+
+- Rewrite of the entire library
+- Much easier initialization
+- Support for conditions
+- Stable HoneyTalk implementation
+
+## 0.0.5
+
 - Support for regex widget names
 - Support for `starts with`, `ends with`, `contains` and `matches` expressions
 - Appium runner
 
 ## 0.0.2
+
 - Bugfixes
 - `optional` keyword
+
 ## 0.0.1
+
 - Initial version
--- honey_vscode/syntaxes/build_tmlanguage.mjs
+++ honey_vscode/syntaxes/build_tmlanguage.mjs
@@ -1,9 +1,7 @@
 import * as yaml from 'js-yaml'
 import * as fs from 'fs'
 
-type VariableReplacer = [RegExp, string];
-
-function replacePatternVariables(pattern: string, variableReplacers: VariableReplacer[]) {
+function replacePatternVariables(pattern, variableReplacers) {
     let result = pattern;
     for (const [variableName, value] of variableReplacers) {
         result = result.replace(variableName, value);
@@ -13,16 +11,16 @@ function replacePatternVariables(pattern: string, variableReplacers: VariableRep
 
 function build() {
     var yamlSource = fs.readFileSync('./syntaxes/honeytalk.yaml', 'utf8')
-    const sourceYaml: any = yaml.load(yamlSource)
+    const sourceYaml = yaml.load(yamlSource)
     const vars = sourceYaml.variables
     
-    const variableReplacers: VariableReplacer[] = [];
+    const variableReplacers = [];
     for (const variableName in vars) {
         const pattern = replacePatternVariables(vars[variableName], variableReplacers);
         variableReplacers.push([new RegExp(`\\$${variableName}`, "gim"), pattern]);
     }
     const result = replacePatternVariables(yamlSource, variableReplacers);
-    const resultYaml: any = yaml.load(result)
+    const resultYaml = yaml.load(result)
     delete resultYaml.variables
     fs.writeFileSync('./syntaxes/honeytalk.json', JSON.stringify(resultYaml, null, 2))
 }
